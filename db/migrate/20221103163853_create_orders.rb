901496e0232b875e96585f6857d6a2ae6ec30d15
juancvillamoros <jvillamoros@outlook.com>
2022-11-03 12:04:58 -0500

    Creating models and controllers with scaffold. Add gem twitter-bootstrap too.

---
+++ db/migrate/20221103163853_create_orders.rb
@@ -0,0 +1,11 @@
+class CreateOrders < ActiveRecord::Migration[7.0]
+  def change
+    create_table :orders do |t|
+      t.float :subtotal
+      t.float :total
+      t.float :tax
+
+      t.timestamps
+    end
+  end
+end
